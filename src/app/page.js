195f94f3e7cac28bb064d4124f02e4fec53b797b
jaychodvadiy <jaychodvadiya130@gmail.com>
2024-01-24 18:34:49 +0530

    digner and herder and slider responsive

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,21 @@
 import Image from "next/image";
 import "../../css/thiyo.css";
 import ThiyoLogo from "../../img/thiyologo.png";
-import home from "../../img/pexelsexpect.png";
+import Asset28 from "../../img/Asset 2 8.png";
+import Asset from "../../img/Asset 3.png";
+import Asset3 from "../../img/Asset 5 3.png";
+import highangle from "../../img/high-angle-pie-chart-with-cities 1.png";
+import airport from "../../img/airport.abbe0c1a.png";
+import apartment from "../../img/apartment.e4223b32.png";
+import hotel from "../../img/hotel.43844a6a.png";
+import office from "../../img/office.6c982fe1.png";
+import penthouse from "../../img/penthouse.c764f65d.png";
+import hotelapartment from "../../img/hotelapartment.ab759b89.png";
+import office6c982fe1 from "../../img/office.6c982fe1.png";
+import shop from "../../img/shop.b3cf7676.png";
+import townshipfb52778a from "../../img/township.fb52778a.png";
+import township from "../../img/villa.665fd0ad.png";
+import vector from "../../img/vector-94.svg";
 
 export default function Home() {
   return (
@@ -93,7 +107,6 @@ export default function Home() {
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
               >
-                {" "}
                 For Tenants
               </a>
               <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -104,20 +117,19 @@ export default function Home() {
                 </li>
               </ul>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <a
-                class="nav-link disabled"
+                className="nav-link disabled"
                 href="#"
                 tabindex="-1"
                 aria-disabled="true"
               >
-                {" "}
                 Insights{" "}
               </a>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <a
-                class="nav-link disabled"
+                className="nav-link disabled"
                 href="#"
                 tabindex="-1"
                 aria-disabled="true"
@@ -126,17 +138,17 @@ export default function Home() {
               </a>
             </li>
           </ul>
-          <form class="d-flex ">
-            <p class=" btn-outline-success" type="submit">
+          <form className="d-flex ">
+            <p className=" btn-outline-success" type="submit">
               Log In/
             </p>
-            <p class=" btn-outline-success" type="submit">
+            <p className=" btn-outline-success" type="submit">
               Register
             </p>
           </form>
         </div>
       </nav>
-      <section className="pagerent">
+      {/* <section className="pagerent">
         <div className="container">
           <div className="row">
             <div className="col-lg-6">
@@ -147,7 +159,7 @@ export default function Home() {
                 <ul className="nav nav-pills" id="pllis-tab" role="tablist">
                   <li className="nav-item" role="presentation">
                     <button
-                      class="text-white search active"
+                      className="text-white search active"
                       id="pills-home-tab"
                       data-bs-toggle="pill"
                       data-bs-target="#pills-home"
@@ -184,6 +196,237 @@ export default function Home() {
                 width={424}
                 height={407}
               />
+            </div>
+          </div>
+        </div>
+      </section> */}
+
+      <section className="feature-home-main">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="homefeaty">
+                <p> FEATURED HOMES</p>
+              </div>
+              <h3 className="ourlest">
+                Explore our latest properties for sale and to rent
+              </h3>
+              <p className="discover">
+                Discover a range of homes available for sale and rent on our
+                platform. From luxurious apartments to spacious houses,
+                <br /> our collection features the latest listings in
+                sought-after locations. Browse through our extensive inventory
+                and find the <br /> perfect home that meets your preferences and
+                budget. Our dedicated team is here to assist you every step of
+                the way.
+                <br /> Start your search today and unlock the door to your ideal
+                property.
+              </p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <div className="nav-align-top tabcountry cstm-tab-main-feature-home">
+                <ul className="nav nav-pills nav-fill" role="tablist">
+                  <li className="nav-item" role="presentation">
+                    <button
+                      type="button"
+                      className="nav-link active"
+                      role="tab"
+                      data-bs-toggle="tab"
+                      data-bs-target="#abu-dhabi"
+                      aria-controls="abu-dhabi"
+                      aria-selected="true"
+                    >
+                      ABU DHABI
+                    </button>
+                  </li>
+                  <li className="nav-item" role="presentation">
+                    <button
+                      type="button"
+                      className="nav-link"
+                      role="tab"
+                      data-bs-toggle="tab"
+                      data-bs-target="#dubai"
+                      aria-controls="dubai"
+                      aria-selected="false"
+                      tabindex="-1"
+                    >
+                      DUBAI
+                    </button>
+                  </li>
+                  <li className="nav-item" role="presentation">
+                    <button
+                      type="button"
+                      className="nav-link"
+                      role="tab"
+                      data-bs-toggle="tab"
+                      data-bs-target="#sharjha"
+                      aria-controls="sharjha"
+                      aria-selected="false"
+                      tabindex="-1"
+                    >
+                      SHARJAH
+                    </button>
+                  </li>
+                  <li className="nav-item" role="presentation">
+                    <button
+                      type="button"
+                      className="nav-link"
+                      role="tab"
+                      data-bs-toggle="tab"
+                      data-bs-target="#ajman"
+                      aria-controls="ajman"
+                      aria-selected="false"
+                      ndex="-1"
+                      tabindex="-1"
+                    >
+                      AJMAN
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="buy-rent-sell-mmain">
+        <div className="container">
+          <div className="row ">
+            <div className="col-lg-12">
+              <div className="baynoe">
+                <p>BUY . RENT . SELL</p>
+                <h2 className="help">
+                  We help people buy, rent and sell homes
+                </h2>
+                <p className="makereal">
+                  We're here to make real estate easy. Whether you're buying,
+                  renting, or selling, our dedicated team is ready to assist you
+                  every step of the way. With our expertise and network, we
+                  ensure a <br />
+                  seamless experience.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-lg-4">
+              <Image
+                src={Asset28}
+                alt="Asset28"
+                className="imgfluid img-fluid"
+              />
+              <h3 className="mb-1 mb-md-3 text-center">Qualified Agents</h3>
+              <p className="pb-0 sellhomedescription">
+                When it comes to finding your drem property or selling your
+                home, trust in our team of highly qualified agents. Our experts
+                bring years of industry experience and deep knowledge of the
+                local real estate market to the table.
+              </p>
+            </div>
+            <div className="col-lg-4">
+              <Image className="imgfluid" src={Asset} alt="Asset" />
+              <h3 className="mb-1 mb-md-3 text-center">Excellent service</h3>
+              <p className="pb-0 sellhomedescription">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+                tellus, luctus nec ullamcorper mattis, pulvinar leo.
+              </p>
+            </div>
+            <div className="col-lg-4">
+              <Image className="imgfluid" src={Asset3} alt="Asset3" />
+              <h3 className="mb-1 mb-md-3 text-center">Customer Care</h3>
+              <p className="pb-0 sellhomedescription">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+                tellus, luctus nec ullamcorper mattis, pulvinar leo.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="cta sellhome bg-transparent">
+        <div className="container">
+          <div className="property">
+            <div className="row">
+              <div className="col-sm-6 cta-info ctn">
+                <h3 className="">Looking to sell your home?</h3>
+                <h6 className="">
+                  Unlock a hassle-free selling experience and unleash the real
+                  potential of your cherished property.
+                </h6>
+                <button className="d-none d-md-flex align-items-center justify-content-center thiyo-btn">
+                  LIST YOUR PROPERTY
+                </button>
+                <button className="d-flex align-items-center justify-content-center d-md-none thiyo-btn">
+                  CONTACT US
+                </button>
+              </div>
+              <div className="col-lg-6">
+                <Image src={highangle} alt="highangle" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+          <div className="residental">
+            <div className="imageitemHoele">
+              <div className="img-gallery-info d-flex justify-content-between flex-column ">
+                <h5>Residental</h5>
+                <p>
+                  Buy or sell your home with our agents. House prices,
+                  inspections, negotiations and other services are included in
+                  the house price.
+                </p>
+                <div class="line">
+                  <Image src={vector} alt="vector" />
+                </div>
+              </div>
+              <div className="col-sm-4 ">
+                <Image className="airports" src={airport} alt="airport" />
+              </div>
+              <div className="col-sm-4 ">
+                <Image className="airports" src={apartment} alt="apartment" />
+              </div>
+              <div className="col-sm-4 ">
+                <Image className="airports" src={penthouse} alt="airport" />
+              </div>
+              <div className="col-sm-4 ">
+                <Image className="airports" src={office} alt="airport" />
+              </div>
+              <div className="col-sm-4 ">
+                <Image className="airports" src={hotel} alt="airport" />
+              </div>
+              <div className="col-sm-4 ">
+                <Image className="airports" src={shop} alt="airport" />
+              </div>
+              <div className="col-sm-4 ">
+                <Image className="airports" src={hotelapartment} alt="airport" />
+              </div>
+              <div className="col-sm-4 ">
+                <Image className="airports" src={office6c982fe1} alt="airport" />
+              </div>
+              <div className="col-sm-4 ">
+                <Image className="airports" src={township} alt="airport" />
+              </div>
+              <div className="col-sm-4 ">
+                <Image className="airports" src={townshipfb52778a} alt="airport" />
+              </div>
+              <div className="text-end img-gallery-info d-flex justify-content-between flex-column">
+                <h5>Commercial</h5>
+                <p>
+                  Buy or sell your home with our agents. House prices,
+                  inspections, negotiations and other services are included in
+                  the house price.
+                </p>
+                <div class="line">
+                  <Image src={vector} alt="vector" />
+                </div>
+              </div>
             </div>
           </div>
         </div>
